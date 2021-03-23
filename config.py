9634639935a93e590048cd4de73db2d76158784e
Ankur Saxena <ankiboyz@gmail.com>
2021-03-23 15:56:46 +0530

    Added Configuration Details

--- config.py
+++ config.py
@@ -1,16 +1,16 @@
 # config.py
 
-from os.path import abspath, dirname, join
 import os
 
-_cwd = dirname(abspath(__file__))
+'''Having the DB file at the Current Working Directory'''
+_cwd = os.path.dirname(os.path.abspath(__file__))
 
 '''SECRET_KEY A secret key that will be used for securely signing the session cookie and 
 can be used for any other security related needs by extensions or your application. 
 It should be a long random string of bytes, although unicode is accepted too. For example, 
 copy the output of this to your config:
-
 '''
+
 secretkey = os.urandom(16)
 print(secretkey)
 SECRET_KEY = secretkey
@@ -19,5 +19,5 @@ SECRET_KEY = secretkey
 which can be useful for debugging.
 '''
 
-SQLALCHEMY_DATABASE_URI = 'sqlite:///' + join(_cwd, 'AIUEngine.db')
+SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(_cwd, 'AIUEngine.db')
 SQLALCHEMY_ECHO = True
